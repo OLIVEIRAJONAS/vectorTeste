8711f41e5c6cbabc963694641be5cd6a135c044c
OLIVEIRAJONAS <45247377+OLIVEIRAJONAS@users.noreply.github.com>
2022-05-10 12:52:54 -0300

    Add files via upload

---
+++ VectorTest.API/VectorTest.API/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VectorTest.API.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonIgnore]
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+       
+        [JsonIgnore]
+        public string Audience { get; set; }
+        [JsonIgnore]
+        public string Issuer { get; set; }
+
+        [JsonIgnore]
+        public Guid key { get; set; }
+
+    }
+}
